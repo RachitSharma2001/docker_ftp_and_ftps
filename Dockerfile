06bb8d586598ae3358c9e4881de546103c38322c
RachitSharma2001 <rachitsharma613@gmail.com>
2022-10-26 10:34:09 -0700

    Enabling file writes

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,11 @@
 FROM ubuntu:latest
 
+ENV USER=user \
+    PASS=123 \
+    HOME_DIR=/home/user
+COPY test.py .
 RUN apt-get update && apt-get install -y vsftpd
 COPY src/entrypoint.sh .
 RUN ["chmod", "+x", "./entrypoint.sh"]
 ENTRYPOINT [ "./entrypoint.sh" ]
-EXPOSE 20/tcp 21/tcp 81/tcp 40000-40009/tcp
+EXPOSE 20/tcp 21/tcp 81/tcp 40000-40009/tcp 10090-10100/tcp
--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
-mkdir /home/user
-useradd -p $(echo "123" | openssl passwd -1 -stdin) -d /home/user user
-chown user:user /home/user
+ls .
+mkdir $HOME_DIR
+cp ../../test.py $HOME_DIR
+ls -la $HOME_DIR
+useradd -p $(echo $PASS | openssl passwd -1 -stdin) -d $HOME_DIR $USER
+chown $USER:$USER $HOME_DIR
 #echo "listen=YES\nlisten_ipv6=NO \n write_enable=YES listen_port=81 \n pasv_enable=YES \n pasv_max_port=10100 \n pasv_min_port=10090 \n chroot_local_user=YES \n allow_writeable_chroot=YES" >> /etc/vsftpd.conf
 #echo -e "write_enable=YES\nchroot_local_user=YES\nallow_writeable_chroot=YES" >> /etc/vsftpd.conf
 echo "listen=YES" >> /etc/vsftpd.conf
@@ -10,13 +13,13 @@ echo "listen_ipv6=NO" >> /etc/vsftpd.conf
 echo "write_enable=YES" >> /etc/vsftpd.conf
 echo "chroot_local_user=YES" >> /etc/vsftpd.conf
 echo "allow_writeable_chroot=YES" >> /etc/vsftpd.conf
+echo "pasv_enable=YES" >> /etc/vsftpd.conf
+echo "pasv_max_port=10100" >> /etc/vsftpd.conf
+echo "pasv_min_port=10090" >> /etc/vsftpd.conf
 
 cp /etc/vsftpd.conf /etc/vsftpd_ssl.conf
 
 echo "listen_port=81" >> /etc/vsftpd.conf
-echo "pasv_enable=YES" >> /etc/vsftpd.conf
-echo "pasv_max_port=10100" >> /etc/vsftpd.conf
-echo "pasv_min_port=10090" >> /etc/vsftpd.conf
 
 echo "ssl_enable=YES" >> /etc/vsftpd_ssl.conf
 echo "allow_anon_ssl=NO" >> /etc/vsftpd_ssl.conf
