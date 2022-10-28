03dc86deeaec6ea5ed37cc56a789f0eeb5335da8
RachitSharma2001 <rachitsharma613@gmail.com>
2022-10-28 12:55:41 -0700

    Add End to End Tests

---
+++ tests/test.py
@@ -0,0 +1,16 @@
+from ftplib import FTP
+
+ftp = FTP()
+ftp.debugging = 2
+ftp.connect('localhost', 81)
+ftp.login('rachit', '567')
+print("Done!")
+
+print(ftp.pwd())
+socket = ftp.transfercmd("STOR file_new")
+fobj = socket.makefile('w')
+fobj.write("asdfsdfdsf")
+fobj.close()
+socket.close()
+print("Done again!")
+print(ftp.mlsd())
