ae548b99f73e3f857a890e41e537f93de2f06d53
RachitSharma2001 <rachitsharma613@gmail.com>
2022-10-28 12:55:41 -0700

    Add End to End Tests

---
+++ tests/test_ssl.py
@@ -0,0 +1,18 @@
+from ftplib import FTP_TLS
+
+ftp = FTP_TLS()
+ftp.debugging = 2
+ftp.connect('localhost', 21)
+ftp.login('rachit', '567')
+ftp.prot_p()
+print("Done!")
+
+
+socket = ftp.transfercmd("STOR file")
+fobj = socket.makefile('w')
+fobj.write("asdfsdfdsf")
+fobj.close()
+socket.close()
+print("Done again!")
+
+print(ftp.mlsd())
