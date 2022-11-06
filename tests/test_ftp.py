c4b1fc74b2f67e0a77736b0c4c4d076c6998ed4d
RachitSharma2001 <rachitsharma613@gmail.com>
2022-11-06 10:44:32 -0800

    Add env variables for tests to access

--- tests/test_ftp.py
+++ tests/test_ftp.py
@@ -1,18 +1,24 @@
 from ftplib import FTP
 import pytest 
 import io
+import os
 
 content = "something else something else \n something\n"
+
+USER = os.environ.get('USER')
+PASS = os.environ.get('PASS')
+HOST = os.environ.get('HOST')
+PORT = int(os.environ.get('FTP_PORT'))
 
 @pytest.fixture
 def ftp():
     ftp = FTP()
-    ftp.connect('localhost', 21)
-    ftp.login('user', '123')
+    ftp.connect(HOST, PORT)
+    ftp.login(USER, PASS)
     return ftp
 
 def test_ftp_read_write(ftp):
     ftp.storlines("STOR ftp_file", io.BytesIO(content.encode()))
     read_socket = ftp.transfercmd("RETR ftp_file")
     fobj = read_socket.makefile('r')
-    assert fobj.read() == content   
+    assert fobj.read() == content 
